853dd13d16394ea5e5dc2f7a45ca08e044210b76
Daniel Ypsilanti <dypsilanti@gmail.com>
2025-02-01 21:37:06 -0500

    Refactor Navbar component layout for improved responsiveness and styling

--- gpf-next/src/app/components/Navbar.tsx
+++ gpf-next/src/app/components/Navbar.tsx
@@ -4,18 +4,18 @@ import Link from 'next/link';
 const Navbar = () => {
   return (
     <nav className="bg-white shadow-md">
-      <div className="container mx-auto flex items-center justify-between p-4">
-        <div className="flex items-center space-x-4">
-          <Image src="/gpf-web-large.png" alt="Logo" width={360} height={0} className="p-4" />
-        </div>
-        <div className="hidden md:flex space-x-6">
-          <Link href="#services" className="text-gray-700 hover:text-gray-900">Services</Link>
-          <Link href="#about" className="text-gray-700 hover:text-gray-900">About Us</Link>
-          <Link href="#contact" className="text-gray-700 hover:text-gray-900">Contact Us</Link>
-        </div>
-        <button className="md:hidden p-2 text-gray-700">☰</button>
-      </div>
-    </nav>
+  <div className="container mx-auto flex flex-col md:flex-row items-center  content-center justify-between p-4">
+    <div className="flex items-center space-x-4">
+      <Image src="/gpf-web-large.png" alt="Logo" width={360} height={0} className="py-4 ps-11 md:ps-0 pe-0 md:w-[18rem]" />
+    </div>
+    <div className="space-y-4 md:space-y-0 space-x-6">
+      <Link href="#services" className="text-gray-700 hover:text-gray-900">Services</Link>
+      <Link href="#about" className="text-gray-700 hover:text-gray-900">About Us</Link>
+      <Link href="#contact" className="text-gray-700 hover:text-gray-900">Contact Us</Link>
+    </div>
+  </div>
+</nav>
+
   );
 };
 
